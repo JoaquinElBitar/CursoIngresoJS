9a69025b1563775e22aa45eb64bfd2fa4c4f9212
JoaquinElBitar <joacozyz@gmail.com>
2023-03-03 07:55:18 -0300

    modificado

--- 8-TPs/12-jsRisingBTL.js
+++ 8-TPs/12-jsRisingBTL.js
@@ -1,4 +1,5 @@
-/*RISING BTL. Empresa dedicada a la toma de datos para realizar estadísticas y censos nos pide realizar una carga de datos validada e ingresada por ventanas emergentes solamente (para evitar hacking y cargas maliciosas) y luego asignarla a cuadros de textos. 
+/*Joaquin ElBitar
+RISING BTL. Empresa dedicada a la toma de datos para realizar estadísticas y censos nos pide realizar una carga de datos validada e ingresada por ventanas emergentes solamente (para evitar hacking y cargas maliciosas) y luego asignarla a cuadros de textos. 
 12.   Los datos requeridos son los siguientes:
 A.	Edad, entre 18 y 90 años inclusive.
 B.	Sexo, “M” para masculino y “F” para femenino
@@ -9,11 +10,58 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
- 	//definicion de variables
- 	var edadIngresada;
- 	var sexoIngresado;
- 	var estadoCivilIngresado;
+	let edad;
+	let sexo;
+	let estadoCivil;
+	let sueldoBruto;
+	let numeroLegajo;
+	let nacionalidad;
+	let respuesta;
 
+	respuesta = "si";
+	
+	while(respuesta == "si" || respuesta == "Si")
+	{
+		//A.	Edad, entre 18 y 90 años inclusive.
+		edad = parseInt(prompt("Ingrese su edad"));
+			while(isNaN(edad) || edad < 17 || edad > 91)
+				{
+					edad = parseInt(prompt("Error: Ingrese una edad valida"));
+				}
+		//B.	Sexo, “M” para masculino y “F” para femenino
+		sexo = prompt("Ingrese su sexo “M” para masculino y “F” para femenino");
+			while(!isNaN(sexo) || sexo != "M" || sexo != "F")
+				{
+					sexo = prompt("Error: Ingrese una sexo valida");
+				}
+		//C.	Estado civil, 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos
+		estadoCivil = prompt("Ingrese su estado civil 1-Soltero, 2-Casados, 3-Divorciados 4-Viudos");
+			while(isNaN(estadoCivil) || estadoCivil != "1" || estadoCivil != "2" || estadoCivil != "3" || estadoCivil != "4")
+				{
+					sexo = parseInt(prompt("Error: Ingrese una nacionalidad valida"));
+				}
+		//D.	Sueldo bruto, no menor a 8000.
+		sueldoBruto = parseInt(prompt("Ingrese su sueldo bruto"));
+			while(isNaN(sueldoBruto) || sueldoBruto < 7999 || sueldoBruto || sueldoBruto > 1400000)
+				{
+					sueldoBruto = parseInt(prompt("Error: Ingrese un sueldo bruto mayor a 8000 "));
+				}
 
+		//E.	Número de legajo, numérico de 4 cifras, sin ceros a la izquierda.		
 
+		//F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para nacionalizados.
+		nacionalidad = prompt("Ingrese su nacionalidad “A”-Argentinos, “E“-Extranjeros, “N”-Nacionalizados");
+			while(!isNaN(nacionalidad) || nacionalidad != "A" || nacionalidad != "E" || nacionalidad != "N")
+				{
+					nacionalidad = parseInt(prompt("Error: Ingrese una nacionalidad valida"));
+				}
+		respuesta = prompt("Desea continuar? Ingrese: Si o No");
+	}//FIN WHILE
+
+	document.getElementById("txtIdEdad").value = edad;
+	document.getElementById("txtIdSexo").value = sexo;
+	document.getElementById("txtIdEstadoCivil").value = estadoCivil;
+	document.getElementById("txtIdSueldo").value = sueldoBruto;
+	document.getElementById("txtIdNacionalidad").value = nacionalidad;
 }
+
--- 9-Parciales/2020_1Cuatri_recu_fuentes/tres.js
+++ 9-Parciales/2020_1Cuatri_recu_fuentes/tres.js
@@ -1,4 +1,135 @@
+/*Joaquin ElBitar
+Enunciado:
+Bienvenidos.
+En el ingreso a un viaje en avión nos solicitan nombre, nacionalidad , edad, sexo("f" o "m") y estado civil("soltero", "casado" o "viudo")y temperatura corporal.
+a) la Nacionalidad de la persona con mas temperatura.
+b) Cuantos mayores de edad( más de 17) estan solteros
+c) La cantidad de Mujeres que hay solteras o viudas.
+d) cuantas personas de la tercera edad( mas de 60 años) , tienen mas de 38 de temperatura
+e) El promedio de edad entre las mujeres casadas.
+*/
 function mostrar()
 {
-	alert("tres");
+    let nombre;
+    let nacionalidad;
+    let edad;
+    let temperatura;
+    let sexo;
+    let respuesta;
+	let estadoCivil;
+	let banderaTemperaturaMasAlta;
+	let nombreTemperaturaMasAlta;
+	let temperaturaMasAlta;
+	let nacionalidadTemperaturaMasAlta;
+	let acumuladorSolteros;
+	let acumuladorCasados;
+	let acumuladorViudos;
+	let contadorSolteros;
+	let contadorCasados;
+	let contadorViudos;
+	let contadorSolterosMayores;
+	let contadorSolterasMujeres;
+	let contadorViudasMujeres;
+	let contadorMujeresCasadas;
+	let acumuladorMujeresCasadas;
+	let contadorTerceraEdad;
+	let promedioMujeresCasadas;
+	let contadorEdadMujeres;
+
+    respuesta = "si"
+	banderaTemperaturaMasAlta = true;
+	acumuladorSolteros = 0;
+	acumuladorCasados = 0;
+	acumuladorViudos = 0;
+	contadorSolteros = 0;
+	contadorCasados = 0;
+	contadorViudos = 0;
+	contadorSolterosMayores = 0;
+	contadorSolterasMujeres = 0;
+	contadorViudasMujeres = 0;
+	contadorMujeresCasadas = 0;
+	acumuladorMujeresCasadas = 0;
+	contadorEdadMujeres = 0;
+
+	while(respuesta == "si" || respuesta == "Si")
+    {
+        nombre = prompt("Ingrese su nombre");
+            while(!isNaN(nombre))
+				{
+					nombre = prompt("Error: Ingrese un nombre valido");
+				}
+		nacionalidad = prompt("Ingrese su nacionalidad ");
+			while(!isNaN(nacionalidad))
+				{
+					nacionalidad = prompt("Error: Ingrese una nacionalidad valida");
+				}
+		edad = parseInt(prompt("Ingrse su edad"));
+            while(isNaN(edad)|| edad < 0 || edad > 108)
+				{
+					edad = prompt("Error: Ingrese una edad valida"); 
+				}
+		sexo = prompt("Ingrese su sexo 'femenino' 'masculino' 'nobinario'");
+            while(sexo != "femenino" || sexo != "masculino" || sexo != "nobinario")
+				{
+					sexo = prompt("Error: Ingrese un sexo valido");
+				}
+		estadoCivil = prompt("Ingrese su estado civil 'soltero', 'casado' o 'viudo'");
+			while(estadoCivil != "soltero" || estadoCivil != "casado" || estadoCivil != "viudo")
+				{
+					nacionalidad = prompt("Error: Ingrese un estado civil valido 'soltero', 'casado' o 'viudo'");
+				}
+        temperatura = parseInt(prompt("Ingrse su temperatura corporal"));
+            while(isNaN(temperatura)|| temperatura < 32 || temperatura > 42)
+				{
+				temperatura = prompt("Error: Ingrese un temperatura corporal valida"); 
+				}
+		switch(estadoCivil)
+			{
+				case "soltero":
+					contadorSolteros = contadorSolteros + 1;
+					acumuladorSolteros + acumuladorSolteros + estadoCivil;
+						if(edad <17)
+							{
+								contadorSolterosMayores = contadorSolterosMayores + 1;
+							}else if(sexo == "femenino")
+								{
+									contadorSolterasMujeres = contadorSolterasMujeres + 1;
+								}
+					break;
+				case "casado":
+					contadorCasados = contadorCasados + 1;
+					acumuladorCasados + acumuladorCasados + estadoCivil;
+						if(sexo == "femenino")
+							{
+								contadorEdadMujeres = contadorEdadMujeres + edad;
+							}
+					break;
+				case "viudo":
+					contadorViudos = contadorViudos + 1;
+					acumuladorViudos + acumuladorViudos + estadoCivil;
+						if(sexo == "femenino")
+							{
+								contadorViudasMujeres = contadorViudasMujeres + 1;
+							}
+					break;
+			}
+		if(banderaTemperaturaMasAlta == true || temperaturaMasAlta > temperatura)
+			{	
+				temperaturaMasAlta = temperatura;
+				nombreTemperaturaMasAlta = nombre;
+				nacionalidadTemperaturaMasAlta = nacionalidad;
+				banderaTemperaturaMasAlta = false;
+			}
+			respuesta = prompt("¿Desea seguir ingresando datos? 'Si' / 'No'");
+		}//FIN WHILE
+
+		promedioMujeresCasadas = acumuladorMujeresCasadas / contadorEdadMujeres;
+
+		if(edad < 60 && temperatura <38)
+			{
+				contadorTerceraEdad = contadorTerceraEdad + 1;
+			}
+		console.log
+		console.log
+		console.log
 }
